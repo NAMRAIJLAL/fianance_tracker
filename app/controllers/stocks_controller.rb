d75f8473885c0cf213f78dfe65e37265ed6e557b
Namra <namraijlal@gmail.com>
2022-10-03 17:38:45 +0500

    Finanace Tracker

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,18 +1,25 @@
 class StocksController < ApplicationController
+
   def search
-		if(params[:stock].present?)
-			@stock = Stock.new_lookup(params[:stock])
-			if @stock
-				respond_to do |format|
-					format.js { render partial: 'users/result' }
-				end
-			else
-				flash.now[:alert] = "Enter valid symbol for stock to search."
-				format.js { render partial: 'users/stock_info'}
-			end
-		else
-			flash.now[:alert] = "Enter symbol for stock to search."
-			format.js { render partial: 'users/stock_info'}
-		end
+    if params[:stock].present?
+      
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        
+        respond_to do |format|
+          format.html { render partial: 'users/result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid symbol to search"
+          format.js { render partial: 'users/result' }
+        end
+      end    
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a symbol to search"
+        format.js { render partial: 'users/result' }
+      end
+    end
   end
 end
